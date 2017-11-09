034a470ffc28dc15dba49a021a90c4a3accda268
kalyanj92 <kalyan.j0824@gmail.com>
2017-11-08 22:57:38 -0600

    Commit for AddSubject.js

--- src/Components/AddSubject.js
+++ src/Components/AddSubject.js
@@ -3,19 +3,26 @@ import React, { Component } from "react";
 class Subjects extends Component {
   constructor (){
     super();
-    newSubject: {}
+    this.state = {
+      newSubject: {}
+    } 
   }
   static defaultProps = {
     groups: ["MPC", "BiPC", "CEC"]
   };
   handleSubmit(e) {
-    console.log(this.refs.group);
-    console.log(this.refs.title.value);
+    // console.log(this.refs.group);
+    // console.log(this.refs.title.value);
     if(this.refs.title.value === ''){
       alert('Please enter a Title value');
     }
     else{
-
+      this.setState({newSubject: {
+        title: this.refs.title.value,
+        group: this.refs.group.value
+      }}, function (){
+        console.log(this.state)
+      });
     }
     console.log('Submitted');
     e.preventDefault();
